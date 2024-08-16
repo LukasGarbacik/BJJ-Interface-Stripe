c2c91b386d16247c65b18f79c3839aec377fc4d3
LukasGarbacik <lukasg@umich.edu>
2024-08-16 15:12:05 -0400

    images and mem

--- app/membership/page.js
+++ app/membership/page.js
@@ -1,9 +1,24 @@
-// app/membership/page.js
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton, Avatar} from '@mui/material';
+'use client'
 
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Box, Grid, Paper, IconButton, Avatar, Menu, MenuItem } from '@mui/material';
+import { useState } from 'react';
 
 const MembershipPage = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const menuId = 'profile-menu';
+
   return (
     <>
       <AppBar position="static" sx={{ bgcolor: '#00274C' }}>
@@ -17,17 +32,58 @@ const MembershipPage = () => {
           <IconButton
             edge="end"
             color="inherit"
+            onClick={handleProfileMenuOpen}
             sx={{ ml: 2 }}
           >
             <Avatar />
           </IconButton>
         </Toolbar>
       </AppBar>
-      <div>
-        <Typography variant="h4" sx={{ textAlign: 'center', mt: 4 }}>
-          Membership Page
-        </Typography>
-      </div>
+
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={isMenuOpen}
+        onClose={handleMenuClose}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+      </Menu>
+
+      <Grid container sx={{ height: 'calc(100vh - 64px)', overflow: 'hidden', alignItems: 'center', bgcolor: 'grey' }}>
+        <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', p: 2 }}>
+          <Box
+            component="img"
+            src="/images/gi.png" // Placeholder for your BJJ Gi image
+            alt="BJJ Gi"
+            sx={{
+              width: '80%',
+              objectFit: 'contain',
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} sx={{ pr: 2, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+          <Paper elevation={3} sx={{ p: 4, width: '70%', textAlign: 'center', mb: 20,  boxShadow: '14px 14px 8px rgba(49, 54, 56, 0.6)' }}>
+            <Typography variant="h2" gutterBottom>
+              5$ Semester Membership
+            </Typography>
+          </Paper>
+          <Paper elevation={3} sx={{ p: 4, width: '80%', textAlign: 'center' }}>
+            <Button variant="contained" color="primary" fullWidth sx={{ bgcolor: '#FFCB05', color: '#00274C' }}>
+              Pay Here
+            </Button>
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 };
